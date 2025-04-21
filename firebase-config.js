f58b81c9ffdf4833522aee404ca0c49c6211dbc6
D4n13lL0pix <danokali04@gmail.com>
2025-04-21 00:58:11 -0400

    changes

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCe42_QRz20WweFOuobwbZpEtFv1YIFmlQ",
+    authDomain: "safeme-61e9a.firebaseapp.com",
+    projectId: "safeme-61e9a",
+    storageBucket: "safeme-61e9a.firebasestorage.app",
+    messagingSenderId: "533601689310",
+    appId: "1:533601689310:web:03457b7cac8fe4accfec4f",
+    measurementId: "G-TDYBHR820F"
+  };
+  const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
